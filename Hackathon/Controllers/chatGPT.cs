8e4c734478fb70e6fe8b94b63f3f111241119fcf
Elmer Figueroa <elmer.figueroa@concentrix.com>
2023-04-11 12:05:06 -0600

    init repository

---
+++ Hackathon/Controllers/chatGPT.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+using OpenAI_API;
+using Hackathon.Models;
+
+namespace Hackathon.Controllers;
+
+public class chatGPT : Controller
+{
+    [Route("promptText")]
+    [HttpPost]
+    public IActionResult promptText(chatGPTModel chatGPT)
+    {
+        var openai = new OpenAIAPI("chatGPTkey");
+        var completions = openai.Completions.CreateCompletionAsync(
+            prompt: chatGPT.Prompt,
+            model: "text-davinci-002",
+            max_tokens: chatGPT.MaxTokens,
+            temperature: chatGPT.Temperature
+        );
+        var response = string.Empty;
+        foreach (var completion in completions.Result.Completions)
+        {
+            response += completion.Text;
+        }
+        return Ok(response);
+    }
+}
---
+++ Hackathon/Models/chatGPTModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Hackathon.Models;
+
+public class chatGPTModel
+{
+    public string? Prompt { get; set; }
+    public int MaxTokens { get; set; } = 100;
+    public float Temperature { get; set; } = 0.5f;
+}
